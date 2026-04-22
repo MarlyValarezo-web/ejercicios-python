1237abd3bee7145b0eee1bec1971b8ee890c4011
MarlyValarezo-web <marlyvalarezoalvaradomarly@gmail.com>
2026-04-22 12:41:07 -0500

    12:41

---
+++ Ejercicio 5_condicionales.py
@@ -0,0 +1,11 @@
+a = 0
+if a > 0 and a %2 ==0:
+    print('A is a positive even number')
+elif a > 0:
+    print('A is a positive odd number')
+elif a < 0:
+    print('A is a negative number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is zero')
